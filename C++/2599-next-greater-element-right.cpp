9c1270274cbb35ddb7286fbcb8d78fc9f6f48403
Sane Aalam <123074523+sane-aalam@users.noreply.github.com>
2023-03-01 18:20:58 +0530

    Add files via upload

---
+++ C++/2599-next-greater-element-right.cpp
@@ -0,0 +1,64 @@
+
+
+// when you are solving any problem which problem base on the j = i + 1 
+// means second loop depnends of the first loop 
+// think about the stack data structure 
+// complexity - 0(N)
+
+/*
+
+
+Input: 
+N = 4, arr[] = [1 3 2 4]
+Output:
+3 4 4 -1
+Explanation:
+In the array, the next larger element 
+to 1 is 3 , 3 is 4 , 2 is 4 and for 4 ? 
+since it doesn't exist, it is -1.
+
+
+*/
+
+
+class Solution{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        
+      stack<long long>s;
+        vector<long long>v;
+        
+        for(int i = n-1; i>= 0; i--){
+            
+            // case -1 stack is emmpty
+            if(s.size() == 0){
+                v.push_back(-1);
+            }
+            // case -2 stack is not empty and stack of top element is greather of the array of element
+            else if(s.size() > 0 && s.top() > arr[i]){
+                v.push_back(s.top());
+            }
+            // case - 3 stack is not emepty but stack of top element is greater not 
+            // then we can pop element and find out the greater of the top
+            else if(s.size() > 0 && s.top() <= arr[i]){
+                
+                while(s.size() > 0 && s.top() <= arr[i]){
+                    s.pop();
+                }
+                // now we have two choice either stack would be empty 
+                // either stack top element is greather to the array of element 
+                if(s.size() == 0){
+                    v.push_back(-1);
+                }else if(s.top() > arr[i]){
+                    v.push_back(s.top());
+                }
+            }
+            // when all the process done then we can push the array element into the stack 
+            s.push(arr[i]);
+        }
+        // we have reverse order vector ( ans array ) in order to maintain correct answer we, need to reverse the array 
+        reverse(v.begin() ,v.end() );
+        return v;
+    }
+};
